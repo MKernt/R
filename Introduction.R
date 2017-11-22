4cee6212ab0ca8779a878d0e4064e95ec1013ce0
Martin Kerntopf <martin.kerntopf@gmail.com>
2017-11-22 10:28:03 +0100

    Created the Introduction.R set

---
+++ Introduction.R
@@ -0,0 +1,14 @@
+##############################
+#                            #
+#   A small R Introduction   #
+#    by Martin Kerntopf      #
+#                            #
+##############################
+
+### 1. Introduction
+
+
+# install.packages("tidyverse") # See also http://r4ds.had.co.nz/
+library(tidyverse)
+
+
